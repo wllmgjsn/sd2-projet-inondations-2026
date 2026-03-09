a349a69631053592e4d28af5f074c17908393247
cindy-tran-vinci <cindy.tran@student.vinci.be>
2026-03-09 12:10:03 +0100

    Add attributs in Arc.java

--- src/Arc.java
+++ src/Arc.java
@@ -1,4 +1,15 @@
 package src;
 
 public class Arc {
+    Noeud origine;
+    Noeud arrivee;
+    double distance;
+    String nomRue;
+
+    public Arc(Noeud origine, Noeud arrivee, double distance, String nomRue) {
+        this.origine = origine;
+        this.arrivee = arrivee;
+        this.distance = distance;
+        this.nomRue = nomRue;
+    }
 }
