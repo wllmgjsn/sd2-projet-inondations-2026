1dfe847c6571a71943bc057277e1cfeb91f78360
kim-bryere-vinci <kim.bryere@student.vinci.be>
2026-03-09 11:16:59 +0100

    add module src

---
+++ src/src/test/java/be/vinci/cae/src/SrcApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.cae.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
