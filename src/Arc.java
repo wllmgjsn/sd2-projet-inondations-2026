e8cee6e0be74ddd674362a8ee7e570502ca65110
vinci-kim-bryere <kim.bryere@student.vinci.be>
2026-03-09 12:10:57 +0100

    add set get arc

--- src/Arc.java
+++ src/Arc.java
@@ -12,4 +12,36 @@ public class Arc {
         this.distance = distance;
         this.nomRue = nomRue;
     }
+
+  public Noeud getArrivee() {
+    return arrivee;
+  }
+
+  public void setArrivee(Noeud arrivee) {
+    this.arrivee = arrivee;
+  }
+
+  public double getDistance() {
+    return distance;
+  }
+
+  public void setDistance(double distance) {
+    this.distance = distance;
+  }
+
+  public String getNomRue() {
+    return nomRue;
+  }
+
+  public void setNomRue(String nomRue) {
+    this.nomRue = nomRue;
+  }
+
+  public Noeud getOrigine() {
+    return origine;
+  }
+
+  public void setOrigine(Noeud origine) {
+    this.origine = origine;
+  }
 }
