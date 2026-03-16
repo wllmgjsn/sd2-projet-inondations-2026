df887811d26b3c54140e850db61e1a2890cab1b3
vinci-william-gijsen <william.gijsen@student.vinci.be>
2026-03-16 12:31:27 +0100

    some changes lol

--- src/Graph.java
+++ src/Graph.java
@@ -1,20 +1,51 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Graph {
-    private Map<Localisation, Set<Arc>> listeRoadLocalisation;
-
-
+    private final Map<Localisation, Set<Arc>> listeRoadLocalisation;
 
     //ATTRIBUT ?
     //TODO
 
     public Graph(String localisations, String roads)  {
+        this.listeRoadLocalisation = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(localisations))) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] loca = line.split(",");
+                Long id       = Long.parseLong(loca[0].trim());
+                String nom    = loca[1].trim();
+                double latitude  = Long.parseLong(loca[2].trim());
+                double longitude = Long.parseLong(loca[3].trim());
+                double altitude  = Long.parseLong(loca[4].trim());
 
-        //TODO
+                Localisation newLoc = new Localisation(id, nom, latitude, longitude, altitude);
+                listeRoadLocalisation.put(newLoc, new HashSet<>());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(roads))) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] road = line.split(",");
+
+
+            }
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
     }
 
     public Localisation[] determinerZoneInondee(long[] idsOrigin, double epsilon) {
