e37cfe35006281971231782e6cac396c84da9784
vinci-kim-bryere <kim.bryere@student.vinci.be>
2026-03-16 11:38:36 +0100

    add: structure

--- src/Graph.java
+++ src/Graph.java
@@ -1,22 +1,32 @@
+
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Map;
+import java.util.Set;
 
 public class Graph {
+    private Map<Localisation, Set<Arc>> listeRoadLocalisation;
 
-	//ATTRIBUT ?
-	//TODO
+
+
+    //ATTRIBUT ?
+    //TODO
 
     public Graph(String localisations, String roads)  {
+
         //TODO
     }
 
-    public Localisation[] determinerZoneInondee(long[] idsOrigin,double epsilon) {
+    public Localisation[] determinerZoneInondee(long[] idsOrigin, double epsilon) {
+        ArrayDeque<Localisation> file;
+        Set<Localisation> dejaVisite;
+
         //TODO
-		return null ;
+        return null ;
     }
 
     public Deque<Localisation> trouverCheminLePlusCourtPourContournerLaZoneInondee(long idOrigin, long idDestination, Localisation[] floodedZone) {
-		//TODO
+        //TODO
         return null ;
     }
 
@@ -27,7 +37,7 @@ public class Graph {
 
     public Deque<Localisation> trouverCheminDEvacuationLePlusCourt(long idOrigin, long idEvacuation, double vVehicule, Map<Localisation,Double> tFlood) {
         //TODO
-		return null ;
+        return null ;
     }
 
 
