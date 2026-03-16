09a937c61595598e9708cbb32ce7273cef6f813e
vinci-william-gijsen <william.gijsen@student.vinci.be>
2026-03-16 12:24:34 +0100

    fix : replaced all tests with the newer ones

--- src/TestSimulator1000a.java
+++ src/TestSimulator1000a.java
@@ -1,10 +1,13 @@
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.Map;
 
 public class TestSimulator1000a {
     public static void main(String[] args) {
+        double epsilon = 0.1 ;
+        double k = 0.05 ;
         System.out.println("---------------------------------------------------------------");
         LocalTime start = LocalTime.now() ;
         Graph graph = new Graph("nodes_1000.csv","edges_1000.csv");
@@ -17,7 +20,7 @@ public class TestSimulator1000a {
         System.out.println("---------------------------------------------------------------");
         long idDepartCrue = 5437074483l ;
         start = LocalTime.now() ;
-        Localisation[] zoneInondee = graph.determinerZoneInondee(new long[]{idDepartCrue});
+        Localisation[] zoneInondee = graph.determinerZoneInondee(new long[]{idDepartCrue},epsilon);
         LocalTime tempCalculZoneInondée = LocalTime.now() ;
         d = Duration.between(start, tempCalculZoneInondée);
         System.out.println();
@@ -51,7 +54,7 @@ public class TestSimulator1000a {
         System.out.println("---------------------------------------------------------------");
         System.out.println();
         start = LocalTime.now() ;
-        Map<Localisation,Double> tFlood =  graph.determinerChronologieDeLaCrue(new long[]{idDepartCrue},0);
+        Map<Localisation,Double> tFlood =  graph.determinerChronologieDeLaCrue(new long[]{idDepartCrue},0,k);
         LocalTime tempsDeterminationChronologieDeLaCrue = LocalTime.now() ;
         d = Duration.between(start,tempsDeterminationChronologieDeLaCrue);
         System.out.println("Temps de calcul pour la chronologie de la crue : "+d.toMinutes()+"m"+d.toSecondsPart()+"s"+d.toNanosPart()+"ns");
