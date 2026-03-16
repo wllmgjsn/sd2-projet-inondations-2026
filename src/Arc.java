3fdf8fb499e165ed13534218c07220528acb22ab
vinci-kim-bryere <kim.bryere@student.vinci.be>
2026-03-16 10:55:10 +0100

    add : arc

---
+++ src/Arc.java
@@ -0,0 +1,47 @@
+package src;
+
+public class Arc {
+  Localisation origine;
+  Localisation arrivee;
+  double distance;
+  String nomRue;
+
+  public Arc(Localisation origine, Localisation arrivee, double distance, String nomRue) {
+    this.origine = origine;
+    this.arrivee = arrivee;
+    this.distance = distance;
+    this.nomRue = nomRue;
+  }
+
+  public Localisation getArrivee() {
+    return arrivee;
+  }
+
+  public void setArrivee(Localisation arrivee) {
+    this.arrivee = arrivee;
+  }
+
+  public double getDistance() {
+    return distance;
+  }
+
+  public void setDistance(double distance) {
+    this.distance = distance;
+  }
+
+  public String getNomRue() {
+    return nomRue;
+  }
+
+  public void setNomRue(String nomRue) {
+    this.nomRue = nomRue;
+  }
+
+  public Localisation getOrigine() {
+    return origine;
+  }
+
+  public void setOrigine(Localisation origine) {
+    this.origine = origine;
+  }
+}
